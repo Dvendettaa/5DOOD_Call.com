494d4b8824aa30d141d4c14a7c4d5ea3ea6220be
Dvendettaa <62357513+Dvendettaa@users.noreply.github.com>
2020-07-22 01:11:58 +0200

    uploading website

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> 5DOOOOD Website </title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+    </head>
+    <body class="header">
+        <h1 class="my-element text"> Welcome to Ptatys World</h1>
+        <div>
+            <div id="particles-js">
+
+                <div class="container">
+                    <h3> 5DOD PTATYS => </h3>
+                    <img src="img/pic3.jpeg">
+                    <img src="img/pic4.jpeg">
+                    <img src="img/pic5.jpeg">
+                    <img src="img/pic10.jpg">
+                </div>
+
+                <div class="container">
+                    <h3>SHMEXY PTATYS => </h3>
+                    <img src="img/pic6.jpeg">
+                    <img src="img/pic7.jpeg">
+                    <img src="img/pic8.jpeg">
+                    <img src="img/pic9.jpeg">
+                </div>
+
+                <div class="button">
+                    <a href="touch.html">TOUCH 5DOD</a> <br> <br>
+                    <a href="tf3ys.html">TF3YS PTATYS</a>
+                </div>
+
+                <footer class="text2">
+                    <h1 class="my-element2"> It is me and you against the World! <3</h1> <br>
+                    <h1 class="my-element3"> <strong> A </strong> </h1>
+                </footer>
+            </div>
+        </div>
+
+        <script src="particles.js"></script>
+        <script src="app.js"></script>
+    </body>
+</html>
+
